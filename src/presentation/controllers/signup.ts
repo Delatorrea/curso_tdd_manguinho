e6b91c15802fb7fd8814b8a53df46f47f776d104
Emerson Delatorre <emerson.delatorre@avanade.com>
2022-12-25 17:34:14 -0300

    feature: ensure SignUpController return error if no password confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '@src/presentation/helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse {
-    const requiredFiled = ['name', 'email', 'password']
+    const requiredFiled = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFiled) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
