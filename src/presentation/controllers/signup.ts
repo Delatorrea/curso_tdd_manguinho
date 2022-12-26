5e3ed0c0983173ed0460b12dc4ca43c94dc1aab3
Emerson Delatorre <emerson.delatorre@avanade.com>
2022-12-26 11:36:09 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,24 +4,32 @@ import { InvalidParamError } from '@src/presentation/errors/invalid-param-error'
 import { badRequest } from '@src/presentation/helpers/http-helper'
 import { Controller } from '@src/presentation/protocols/controller'
 import { EmailValidator } from '@src/presentation/protocols/email-validator'
+import { ServerError } from '@src/presentation/errors/server-error'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (private readonly emailValidator: EmailValidator) { }
 
   handle (httpRequest: httpRequest): httpResponse {
-    const requiredFiled = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFiled) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFiled = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFiled) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return {
-      statusCode: 200,
-      body: {}
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
